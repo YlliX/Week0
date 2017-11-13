216399b1bbc89b94716a126a9ebc6672f96060b2
YlliX <33637675+YlliX@users.noreply.github.com>
2017-11-13 23:32:46 +0100

    Add files via upload

---
+++ Quote.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+// this is a comment
+func main() {
+fmt.Println("You don't develop courage by being happy in your relationships everyday. You develop it by surviving difficult times and challenging adversity")
+}
