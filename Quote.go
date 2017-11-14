83f0fd6142d86afa33e9f69dabc0a82262f617b4
YlliX <33637675+YlliX@users.noreply.github.com>
2017-11-14 21:54:10 +0100

    Update Quote.go

--- Quote.go
+++ Quote.go
@@ -2,5 +2,6 @@ package main
 import "fmt"
 // this is a comment
 func main() {
-fmt.Println("You don't develop courage by being happy in your relationships everyday. You develop it by surviving difficult times and challenging adversity")
+fmt.Println("\"You don't develop courage by being happy in your relationships everyday. You develop it by surviving difficult times and challenging adversity\"")
+fmt.Println("~Epicurus")
 }
